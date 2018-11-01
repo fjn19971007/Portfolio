b4184f87769825d98943e7a0d70b8098293c81c3
Jianan Fang <fjn19971007@outlook.com>
2018-11-01 01:46:05 -0500

    edit resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -49,66 +49,79 @@ class Resume extends Component {
         <p className={classes.paragraph}>
           University of Minnesota - Twin Cities <br/>
           B.S. in Computer Science <br/>
-          Sep 2015 - May 2019 <br/>
-          Technical GPA: 3.6 <br/>
+          September 2015 - May 2019 <br/>
+          Technical GPA: 3.6/4.0 <br/>
         </p>
-        <div className={classes.caption}>TECHNICAL SKILLS</div>
+        <div className={classes.caption}>SKILLS</div>
           <p className={classes.subtitle} >Languages:</p>
             <p className={classes.paragraph}>
-              JavaScript(3yrs) • HTML(3yrs) • CSS(3yrs) • Java(2yrs) •<br/>
-              Python(2yrs) • PHP • C • C++ <br/>
+              JavaScript(3yrs), HTML(3yrs), CSS(3yrs), Python(2yrs), Java(2yrs),<br/> PHP, C, C++
             </p>
           <p className={classes.subtitle}>Tools & Frameworks:</p>
             <p className={classes.paragraph}>
-              Node.js • React • Redux • AWS • GraphQL • WordPress •<br/>
-              MySQL • Bootstrap • Twilio • Material Design • Git
+              Node.js, React, Redux, MySQL, PostgreSQL, MongoDB, AWS, <br/>
+              GraphQL, Bootstrap, Express, Flask, Ajax, jQuery, git
             </p>
         <div className={classes.caption}>EXPERIENCES</div>
-          <p className={classes.subtitle}>Full-Stack Software Development Intern</p>
+        <p className={classes.subtitle}>Software Engineer Intern</p>
+          <p className={classes.paragraph}>Sleep Number<br/>
+            September 2018 - Present</p>
+          <p className={classes.paragraph}>
+            • Improving the Live Chat life cycle and modeling a Chat Bot for customer services team in Sleep Number. <br/>
+            • Using Google Dialog Flow and machine learning technics on enormous live chat data to train the Chat Bot. <br/>
+            • The improvement will affect millions of live chat conversation ongoing each year in Sleep Number. <br/>
+          </p>
+          <p className={classes.subtitle}>Full-Stack Software Developer Intern</p>
             <p className={classes.paragraph}>Dose Health<br/>
-              May 2018 - Aug 2018</p>
+              May 2018 - September 2018</p>
             <p className={classes.paragraph}>
-              Constructed a Medication Reminder System that automatically reminds clients to take their medication by
-              phone call, text message and IoT product helping people stay on track with their pills. <br/>
-              Implemented server-less lambda functions instead of backend server. <br/>
-              Designed and implemented a web application that can simply manage data in Medication Reminder System simplifying step of adding and editing users. <br/>
-              Developed 70% of the entire system which became the 2nd key product of the company.
+              • Developed a Medication Reminder System that automatically calls and texts users to take their medication. Designed and implemented web UI that helps co-workers easily add new users into the database. <br/>
+              • Implemented 70% of the entire system front-end to back-end with JavaScript and AWS. Now the system is serving 200+ of users every day helping them keep on track of their medications. <br/>
+              • Created whole server based on AWS Cloud and AWS Lambda serverless architecture which has the ability to auto scale. <br/>
             </p>
           <p className={classes.subtitle}>Research Tech Assistant</p>
             <p className={classes.paragraph}>University of Minnesota<br/>
-              Jan 2018 - May 2018
+              January 2018 - May 2018
             </p>
             <p className={classes.paragraph}>
-              Designed and developed website for Small Island Climate & Health Laboratory (SICHL) department under College of Public
-              Health in University of Minnesota, gained popularity and awareness for their projects. <br/>
-              Embedded Google Map API in the website that marks research details on a global map based on their areas.
+              • Designed and developed a website for Small Island Climate & Health Laboratory (SICHL) in the University of Minnesota. <br/>
+              • The laboratory has gained popularity and awareness from this website that shows geological data of their disasters projects using Google Map API and data provided from research.
             </p>
         <div className={classes.caption}>PROJECTS</div>
+          <p className={classes.subtitle}>Crypto Dashboard</p>
+          <p className={classes.paragraph}>
+            May 2018 - Present
+          </p>
+          <p className={classes.paragraph}>
+            • Developed a crypto dashboard that displays market data and charts of Market Price of 3000+ different types of cryptocurrencies. <br/>
+            • Integrated with HighChart and CryptoCompare to pull data of thousands of crypto coins and visualize it in form of charts.<br/>
+            • Used browser local storage to cache the coins picked by users and show them when user visit the page next time.
+          </p>
+          <p className={classes.subtitle}>Music Rating</p>
+            <p className={classes.paragraph}>
+              September 2018 - Present
+            </p>
+            <p className={classes.paragraph}>
+              • Implemented a music rating web application that users can rate and comment music tracks in it. Based on users’ rating points on tracks, a recommended music list will display on users’ dashboard. <br/>
+              • Used Flask to build the backend server connecting to a PostgreSQL database hosting on Heroku. <br/>
+              • Used music dataset and recommender system from Last.fm API to implement the music recommendation feature.
+            </p>
           <p className={classes.subtitle}>Metro Food Access Network Website</p>
             <p className={classes.paragraph}>
               August 2018 - Present
             </p>
             <p className={classes.paragraph}>
-              Redesigning and rebuilding Metro Food Access Network (MFAN) Website using modern web technology. <br/>
-              The loading performance of new web application was reduced from seconds to milliseconds.
+              • Redesigning and rebuilding Metro Food Access Network (MFAN) Website using modern web technology. <br/>
+              • The loading performance of new web application was reduced from seconds to milliseconds.
             </p>
           <p className={classes.subtitle}>Weather Forecast Web App</p>
             <p className={classes.paragraph}>
               March - April 2018
             </p>
             <p className={classes.paragraph}>
-              Developed a weather forecast website in which users can get weather data from different cities they want. <br/>
-              Charts of temperature, pressure and humidity related to each city will display in the website.
+              • Developed a weather forecast website in which users can get weather data from different cities they want. <br/>
+              • Charts of temperature, pressure and humidity related to each city will display in the website.
             </p>
-          <p className={classes.subtitle}>Cryptocurrency Statistic Website</p>
-            <p className={classes.paragraph}>
-              Dec 2017
-            </p>
-            <p className={classes.paragraph}>
-              Developed and designed a website which displays market data and charts of Market Price of Bitcoin, Litecoin,
-              Ethereum and Bitcoin Cash. <br/>
-              Integrated with Google Chart API and multiple Cryptocurrency Market Price APIs.
-              </p>
     </div>
     );
   }
